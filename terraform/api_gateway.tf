386516f54f5da88fda7b949ac932406f70111ed9
Luyao Wang <lywang88@bu.edu>
2026-03-22 23:42:55 -0700

    - Added new routes for file upload and metadata services in the API Gateway. - Updated integration settings to include backend ALB for traffic forwarding.

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -8,10 +8,8 @@ resource "aws_apigatewayv2_api" "file_service_api" {
 resource "aws_apigatewayv2_integration" "file_lookup_integration" {
 
   api_id = aws_apigatewayv2_api.file_service_api.id
-
   integration_type = "AWS_PROXY"
   integration_uri  = aws_lambda_function.file_lookup.invoke_arn
-
   integration_method = "POST"
 }
 
@@ -19,18 +17,37 @@ resource "aws_apigatewayv2_integration" "file_lookup_integration" {
 resource "aws_apigatewayv2_route" "get_files_route" {
 
   api_id = aws_apigatewayv2_api.file_service_api.id
-
-  route_key = "GET /files"
-
+  route_key = "GET /api/files"
   target = "integrations/${aws_apigatewayv2_integration.file_lookup_integration.id}"
+}
+
+// route for the upload url service
+resource "aws_apigatewayv2_route" "post_upload_url_route" {
+  api_id    = aws_apigatewayv2_api.file_service_api.id
+  route_key = "POST /api/upload/url"
+  target    = "integrations/${aws_apigatewayv2_integration.backend_alb_integration.id}"
+}
+
+// route for the metadata service
+resource "aws_apigatewayv2_route" "post_metadata_route" {
+  api_id    = aws_apigatewayv2_api.file_service_api.id
+  route_key = "POST /api/metadata"
+  target    = "integrations/${aws_apigatewayv2_integration.backend_alb_integration.id}"
 }
 
 #stage for the api gateway
 resource "aws_apigatewayv2_stage" "default_stage" {
 
   api_id = aws_apigatewayv2_api.file_service_api.id
-
   name = "$default"
-
   auto_deploy = true
+}
+
+// integration for the backend ALB to forward the traffic to the backend server
+resource "aws_apigatewayv2_integration" "backend_alb_integration" {
+  api_id                 = aws_apigatewayv2_api.file_service_api.id
+  integration_type       = "HTTP_PROXY"
+  integration_method     = "ANY"
+  integration_uri        = aws_lb_listener.http_listener.arn
+  payload_format_version = "1.0"
 }
